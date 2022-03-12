8b81537f4e9287d1e9510269410caf67b86c29d3
Eduardo Luiz <hello@eduardoluiz.dev>
2022-03-12 02:21:55 -0300

    feat: create user module

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,23 @@
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
+import UsersController from '../controllers/UsersController';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.get('/', isAuthenticated, usersController.index);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+export default usersRouter;
