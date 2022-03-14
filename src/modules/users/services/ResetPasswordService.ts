c10ee2fecdf2318cd942e5af13c578749f5a9cb3
Eduardo Luiz <hello@eduardoluiz.dev>
2022-03-13 22:11:14 -0300

    feat: create forgot and reset password services

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
+import { addHours, isAfter } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import UsersTokensRepository from '../typeorm/repositories/UsersTokensRepository';
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokenRepository = getCustomRepository(UsersTokensRepository);
+
+    const userToken = await userTokenRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User Token does not exists');
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired.');
+    }
+
+    user.password = await hash(password, 8);
+
+    await usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordService;
