5da2ab185d3d8380c5edb05d2761b9d5943e9e31
Eduardo Luiz <hello@eduardoluiz.dev>
2022-03-20 18:04:02 -0300

    feat: protect api for DDOS attacks

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,11 +9,15 @@ import { errors } from 'celebrate';
 import { pagination } from 'typeorm-pagination';
 import cors from 'cors';
 import routes from './routes';
+import rateLimiter from '@shared/http/middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
+
 app.use(pagination);
 app.use('/public', express.static(uploadConfig.directory));
 app.use(routes);
