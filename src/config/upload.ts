347c7b06a5d1dd1fcc1a57a241993a6d58264aaf
Eduardo Luiz <hello@eduardoluiz.dev>
2022-03-22 09:12:33 -0300

    feat: add s3 storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,19 +1,43 @@
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
   directory: uploadFolder,
-  storage: multer.diskStorage({
-    destination: uploadFolder,
-    filename(_req, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
+  tmpFolder,
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(_req, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
 
-      const filename = `${fileHash}-${file.originalname}`;
+        const filename = `${fileHash}-${file.originalname.replace(/\s/g, '-')}`;
 
-      callback(null, filename);
+        callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    aws: {
+      bucket: process.env.AWS_S3_BUCKET_NAME,
     },
-  }),
-};
+  },
+} as IUploadConfig;
--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,10 @@
-import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
-import fs from 'fs';
-import path from 'path';
+import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
+import uploadConfig from '@config/upload';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import S3StorageProvider from '@shared/providers/StorageProvider/S3StorageProvider';
 
 interface IRequest {
   user_id: string;
@@ -14,23 +14,27 @@ interface IRequest {
 class UpdateUserAvatarService {
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
-
     const user = await usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError('User not found.');
     }
 
-    if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
+    if (uploadConfig.driver === 's3') {
+      const s3Provider = new S3StorageProvider();
+      if (user.avatar) {
+        await s3Provider.deleteFile(user.avatar);
       }
+      const filename = await s3Provider.saveFile(avatarFilename);
+      user.avatar = filename;
+    } else {
+      const diskProvider = new DiskStorageProvider();
+      if (user.avatar) {
+        await diskProvider.deleteFile(user.avatar);
+      }
+      const filename = await diskProvider.saveFile(avatarFilename);
+      user.avatar = filename;
     }
-
-    user.avatar = avatarFilename;
 
     await usersRepository.save(user);
 
